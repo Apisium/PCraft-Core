c4ae5a9af3adb3c2b6237f57cdb8fa73a10638fb
Shirasawa <764798966@qq.com>
2018-02-12 23:42:33 +0800

    移除不必要的卸载函数

--- src/main/java/cn/apisium/pcraft/core/EventManager.kt
+++ src/main/java/cn/apisium/pcraft/core/EventManager.kt
@@ -3,6 +3,4 @@ package cn.apisium.pcraft.core
 interface EventManager {
   fun setEmitter (emit: (event: Any) -> Unit)
   fun register (name: String)
-  fun unregister (name: String)
-  fun unregisterAll ()
 }
